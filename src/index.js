6ef31a4ce66a44d8dfbd73d788c2e93fe4e482e7
MaksymAfanasiev <afanasiev.maksym@pdffiller.team>
2021-01-04 23:50:05 +0200

    added eslint and remove node_modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { Command } from 'commander';
+import fs from 'fs';
+import path from 'path';
+
 const program = new Command();
 
 export default () => {
@@ -7,6 +10,13 @@ export default () => {
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
+  .action(function (src1, src2) {
+    const resolvPath1 = path.resolve(process.cwd(), src1)
+    const resolvPath2 = path.resolve(process.cwd(), src2)
+    const file1 = fs.readFileSync(resolvPath1, 'utf-8');
+    const file2 = fs.readFileSync(resolvPath2, 'utf-8');
+    console.log(resolvPath1, resolvPath2);
+  });
 
   program.parse(process.argv);
 }
